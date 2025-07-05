f31a23b804f22fb5710243e8c1395a7ee1bca28a
Damnning <Danning64@gmail.com>
2025-07-05 22:03:19 +0300

    feat: adding abstract repositories

---
+++ src/domain/repository/ResultRepository.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from src.domain.entity.Result import Result
+
+
+class ResultRepository(ABC):
+    @abstractmethod
+    def add_result(self, result: Result) -> None:
+        """Добавить результат"""
+        pass
+
+    @abstractmethod
+    def get_result_by_id(self, result_id: int) -> Result:
+        """Получить результат с id"""
+        pass
+
+    @abstractmethod
+    def get_user_test_result(self, user_id: int, test_id: id) -> Result:
+        """Получить результат пользователя с id по тесту с id"""
+        pass
+
+    @abstractmethod
+    def get_user_result(self, user_id: int) -> List[Result]:
+        """Получить все результаты пользователя с id"""
+        pass
+
+    @abstractmethod
+    def get_test_result(self, test_id: int) -> Result:
+        """Получить все результаты по тесту с id"""
+        pass
+
+    @abstractmethod
+    def delete_result_by_id(self, result_id: int) -> None:
+        """Удалить результат по id"""
+        pass
+
+    @abstractmethod
+    def delete_user_test_result(self, user_id: int, test_id: id) -> None:
+        """Удалить результат пользователя с id по тесту с id"""
+        pass
+
+    @abstractmethod
+    def edit_result_by_id(self, result_id: int, result: Result) -> None:
+        """Изменить вариант с id"""
+        pass
+
+    @abstractmethod
+    def edit_user_test_result(self, user_id: int, test_id: int, result: Result) -> None:
+        """Изменить вариант пользователя с id на тест с id"""
+        pass
+
+    @abstractmethod
+    def get_all_results(self) -> List[Result]:
+        """Получить все результаты"""
+        pass
---
+++ src/domain/repository/TestRepository.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from src.domain.entity.Test import Test
+
+
+class TestRepository(ABC):
+    @abstractmethod
+    def add_test(self, test: Test) -> None:
+        """Удалить тест"""
+
+    @abstractmethod
+    def get_test(self, test_id) -> Test:
+        """Получить тест с id"""
+        pass
+
+    @abstractmethod
+    def delete_test(self, test_id) -> None:
+        """Удалить тест с id"""
+        pass
+
+    @abstractmethod
+    def edit_test(self, test_id, test: Test) -> None:
+        """Изменить тест с id"""
+        pass
+
+    @abstractmethod
+    def get_all_tests(self) -> List[Test]:
+        """Получить все тесты"""
+        pass
