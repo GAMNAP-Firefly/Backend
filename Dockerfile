610fbc1da1424e522bc31ae1499a67b4383a843b
Damnning <Danning64@gmail.com>
2025-07-09 23:04:05 +0300

    fix: changed pydantic to pydantic_settings for BaseSettings import

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ubuntu:latest
-LABEL authors="админ"
-
-ENTRYPOINT ["top", "-b"]
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
 
-from infrastructure.config.settings import settings
+from src.infrastructure.config.settings import settings
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
