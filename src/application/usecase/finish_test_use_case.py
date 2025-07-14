cc00393f82091e0f46feb61d4d18e9a0628abc15
Damnning <Danning64@gmail.com>
2025-07-14 21:02:05 +0300

    fix: finish test endpoint

--- src/application/usecase/finish_test_use_case.py
+++ src/application/usecase/finish_test_use_case.py
@@ -1,18 +1,32 @@
 import uuid
 from datetime import datetime
+from typing import List
+
 from src.application.service.scoring_service import ScoringService
 from src.application.dto.CategoryScoreDTO import CategoryScoreDTO
 from src.application.dto.HRShareLinkDTO import HRShareLinkDTO
+from src.domain.entity.Answer import Answer
+from src.domain.entity.Result import Result
+from src.domain.repository.AnswerRepository import AnswerRepository
+from src.domain.repository.CategoryRepository import CategoryRepository
+from src.domain.repository.QuestionRepository import QuestionRepository
+from src.domain.repository.ResultRepository import ResultRepository
+
 
 class FinishTestUseCase:
     """
     Use case для завершения теста.
     Подсчитывает результаты, генерирует токен-ссылку для HR и обновляет статус сессии.
     """
-    def __init__(self, result_repo, answer_repo, category_repo):
+
+    def __init__(self, result_repo: ResultRepository,
+                 answer_repo: AnswerRepository,
+                 category_repo: CategoryRepository,
+                 question_repo: QuestionRepository):
         self.result_repo = result_repo
         self.answer_repo = answer_repo
         self.category_repo = category_repo
+        self.question_repo = question_repo
 
     async def execute(self, result_id: int) -> tuple[list[CategoryScoreDTO], HRShareLinkDTO]:
         """
@@ -22,12 +36,14 @@ class FinishTestUseCase:
         :return: Кортеж (список DTO с результатами по каждой категории, DTO ссылки для HR).
         """
         # 1. Получаем все необходимые данные из репозиториев
-        result = await self.result_repo.get_result_by_id(result_id)
-        answers = await self.answer_repo.get_answers_by_result(result_id)
-        
+        result: Result = await self.result_repo.get_result_by_id(result_id)
+        answers: List[Answer] = await self.answer_repo.get_answers_by_result(result_id)
+        for answer in answers:
+            question = await self.question_repo.get_question(question_id=answer.question.id)
+            answer.question.scoring_rules = question.scoring_rules
+
         # 2. Выполняем бизнес-логику (подсчет очков)
         scores = ScoringService().calculate_scores(answers)
-
         # 3. Собираем данные для DTO (имена категорий)
         category_ids = list(scores.keys())
         categories = await self.category_repo.get_categories_by_ids(category_ids)
@@ -46,10 +62,10 @@ class FinishTestUseCase:
         result.set_interpretation("[Анализ будет добавлен позже]")
 
         await self.result_repo.edit_result_by_id(result_id, result)
-        
+
         # 6. Создаем DTO для ссылки HR
         hr_share_link = HRShareLinkDTO(share_code=share_code)
-        
+
         # 7. Возвращаем готовые DTO
         category_scores = [
             CategoryScoreDTO(
@@ -57,5 +73,5 @@ class FinishTestUseCase:
                 score=score
             ) for cid, score in scores.items()
         ]
-        
-        return category_scores, hr_share_link 
+
+        return category_scores, hr_share_link
